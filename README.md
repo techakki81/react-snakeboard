d1dd770acb98853578f9cf2962d4af853955fd17
l <aksahy.koulu@example.com>
2021-05-22 18:33:34 +0200

    added

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 In nutshell, We draw a two dimensional grid and then listen to the keyboard arrows.
 Based on these arrows we redraw the square and it looks like a snake moving 
 
+Learning objective: we learn useEffect, Array methods, map, findIndex etc
 Full credit to : https://dev.to/aligumustosun/making-a-snake-game-using-react-hooks-5606
 Twitter :  https://twitter.com/aligumust3
+github :
 
-we learn useEffect, Array methods, map, findIndex etc
+
 steps
 1. creeat the base matrix with blank value array
--- src/App.js
+++ src/App.js
@@ -4,20 +4,15 @@ import snake from  './img/snake.png'
 import './App.css'
 export default function App() {
  
-  let dim= 10
+  const dim= 10
   
   let initialArr = Array.from( {length:dim}, (element, index, array)  => 
                                 Array.from( {length:dim}, (sE,sI,sA)=> 
                                   ({x:index,y:sI})     
   ))
 
- const[selectedSnake, setSelectedSnake] = useState([{x:0,y:0}, {x:0,y:1}])
+ const[selectedSnake, setSelectedSnake] = useState([{x:0,y:0}, {x:0,y:1},{x:0,y:2}])
  
- selectedSnake.findIndex( item =>{
-   //console.log(item)
-   return false 
- })
-
  const getHtml = () => {    
    return initialArr.map( (rootItem) =>{
      // console.log(rootItem)
@@ -45,10 +40,31 @@ export default function App() {
 
  }
   
+ const logKey = (e)=>{
+   switch(e.keyCode){
+     case 38:
+          let newSelectedSnake = selectedSnake;
+          newSelectedSnake.shift()
+          newSelectedSnake.push({x:0,y:3})
+          setSelectedSnake(newSelectedSnake)          
+          break;     // up
+     case 39:     // right 
+     case 40:     // down 
+     case 37:     // left 
+     default:
+       break;
+
+   }
+ }
   useEffect(() => {
-         return () => {     
+         // set the events on window 
+
+         window.addEventListener("keydown", (e)=>logKey(e), false);
+
+         return () => {
+          window.removeEventListener("keydown",(e)=> logKey(e), false);
     }
-  }, [selectedSnake])
+  }, [])
 
  let html =""
   return (
